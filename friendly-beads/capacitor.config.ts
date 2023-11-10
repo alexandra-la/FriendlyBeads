6e0f76b236d560a75d88a84701ae0588a9e3111f
alexandra-la <alexandra@thelamears.com>
2023-11-10 15:15:03 -0600

    Ionic Setup

---
+++ friendly-beads/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Friendly Beads',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
