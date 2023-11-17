951d48edfa508fbef578790319238eb9584ce5a0
alexandra-la <alexandra@thelamears.com>
2023-11-16 23:45:37 -0600

    make signin an official form

--- friendly-beads/src/app/signin/signin.page.ts
+++ friendly-beads/src/app/signin/signin.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NavController } from '@ionic/angular';
+import { SigninPageForm } from './signin.page.form';
 
 @Component({
   selector: 'app-signin',
@@ -7,13 +9,20 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+ //@ts-ignore
+  form: FormGroup;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = new SigninPageForm(this.formBuilder).createForm();
   }
+  //Navigate to Register page
   goToRegister(){
     this.navCtrl.navigateForward('register')
   }
 
+  login(){
+    this.navCtrl.navigateForward('account');
+  }
 }
