e6d56779d470ad0f326178dc144ca4b42419d528
alexandra-la <alexandra@thelamears.com>
2023-11-19 21:51:24 -0600

    user routes set up

--- friendly-backend/api/UserRoutes.js
+++ friendly-backend/api/UserRoutes.js
@@ -9,5 +9,39 @@ router.get('/',(req,res)=>{
     })
     
 })
+router.post('/register',(req,res)=>{
+    if(userExists(req.body.email)){
+        res.status(409).json({error:'Email already exists'})
+    }else{
+        const newUser = new User(req.body)
+        newUser.save().then(user =>{
+            res.status(201).json(user)
+        }).catch(err=>{
+            res.status(500).json({error: err.message})
+        })
+    }
+    
+})
+
+router.post('/login', (req,res)=>{
+    User.findOne({email:req.body.email, password:req.body.password}).then(user =>{
+        if(user){
+            res.status(200).json(user)
+        }else{
+            res.status(401).json({error: 'Incorrect email or password'})
+        }
+    }).catch(err =>{
+        res.status(500).json({error: err.message})
+    })
+})
+
+const userExists = async (email) =>{
+    const user = await User.findOne({email: email.toLowerCase().trim()})
+        if(user){
+            return true
+        }else{
+            return false
+        }
+}
 
 module.exports = router
