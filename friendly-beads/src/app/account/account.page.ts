65c2d4fc8c30de3270193fec4c63572acc9d3c7f
alexandra-la <alexandra@thelamears.com>
2023-11-26 12:24:23 -0600

    account page

--- friendly-beads/src/app/account/account.page.ts
+++ friendly-beads/src/app/account/account.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-account',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
+  goHome(){
+    this.navCtrl.navigateForward('home')
+  }
   ngOnInit() {
   }
 
