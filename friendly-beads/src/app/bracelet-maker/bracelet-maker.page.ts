8544babe698a6c156918569636a2ade9ea099f3d
alexandra-la <alexandra@thelamears.com>
2023-12-07 15:36:04 -0600

    changes

--- friendly-beads/src/app/bracelet-maker/bracelet-maker.page.ts
+++ friendly-beads/src/app/bracelet-maker/bracelet-maker.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import {getAuth} from '@angular/fire/auth';
 import { getApp } from '@angular/fire/app';
 import { addDoc, collection, getFirestore } from '@angular/fire/firestore';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-bracelet-maker',
@@ -14,7 +16,11 @@ export class BraceletMakerPage implements OnInit {
   createBraceletForm!: FormGroup;
   auth = getAuth();
   user = this.auth.currentUser;
-  constructor( private router: Router) { }
+  constructor( private router: Router, private navCtrl: NavController, private readonly loadingCtrl: LoadingController, private readonly alertCtrl: AlertController, formBuilder: FormBuilder, public Fauth: AngularFireAuth) { }
+
+  goHome(){
+    this.navCtrl.navigateForward('home')
+  }
 
   ngOnInit(): void{
     const user = localStorage.getItem('User')
