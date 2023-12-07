0cbc224ad8ab807f4e7791c51031f3ec3ea8a570
alexandra-la <alexandra@thelamears.com>
2023-12-07 15:21:48 -0600

    maybe this will work

---
+++ friendly-beads/src/app/models/bracelet.interface.ts
@@ -0,0 +1,11 @@
+export interface Bracelet {
+  id: string;
+  userEmail: string;
+  name: string;
+  description: string;
+  beadtype: string;
+  colors: Array<string>;
+  letters: string;
+  strands: number;
+  tags: Array<string>;
+}
